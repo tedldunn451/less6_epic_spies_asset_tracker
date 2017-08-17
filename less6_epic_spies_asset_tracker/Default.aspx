5d996b9a8b5b1e9e7e1e365800a5641b54d4f7d5
Ted Dunn <tldunn451@gmail.com>
2017-08-17 16:18:18 -0400

    Add completed project files for lesson 6 Epic Spies Asset Tracker challenge for Tech Academy C# course

---
+++ less6_epic_spies_asset_tracker/Default.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="less6_epic_spies_asset_tracker.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Image ID="spyLogoImage" runat="server" Height="150px" ImageUrl="epic-spies-logo.jpg" />
+            <br />
+            <br />
+            <asp:Label ID="Label1" runat="server" Font-Bold="True" Font-Names="Arial" Font-Size="X-Large" Text="Asset Performance Tracker"></asp:Label>
+            <br />
+            <br />
+            Asset Name:
+            <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Elections Rigged:
+            <asp:TextBox ID="riggedTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            Acts of Subterfuge Performed:
+            <asp:TextBox ID="actsTextBox" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:Button ID="addButton" runat="server" OnClick="addButton_Click" Text="Add Asset" />
+            <br />
+            <br />
+            <asp:Label ID="riggedLabel" runat="server"></asp:Label>
+            <br />
+            <asp:Label ID="averageActsLabel" runat="server"></asp:Label>
+            <br />
+            <asp:Label ID="assetNameLabel" runat="server"></asp:Label>
+        </div>
+    </form>
+</body>
+</html>
---
+++ less6_epic_spies_asset_tracker/Default.aspx.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace less6_epic_spies_asset_tracker
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+                // Create new empty arrays and add them to ViewState to maintain "state" after each button click
+                int[] rigged = new int[0];
+                ViewState.Add("rigged", rigged);
+
+                double[] acts = new double[0];
+                ViewState.Add("acts", acts);
+            }
+        }
+
+        protected void addButton_Click(object sender, EventArgs e)
+        {
+            // Put values stored in ViewState into arrays
+            int[] rigged = (int[])ViewState["rigged"];
+            double[] acts = (double[])ViewState["acts"];
+
+            // Increase the size of each array by 1
+            Array.Resize(ref rigged, rigged.Length + 1);
+            Array.Resize(ref acts, acts.Length + 1);
+
+            // Store the values entered by the user in the text boxes into their appropriate array
+            rigged[rigged.Length - 1] = int.Parse(riggedTextBox.Text);
+            acts[acts.Length - 1] = double.Parse(actsTextBox.Text);
+
+            // Store updated arrays back into ViewState to update "state" of page
+            ViewState["rigged"] = rigged;
+            ViewState["acts"] = acts;
+
+            // Display results into labels on screen
+            riggedLabel.Text = "Total Elections Rigged: " + rigged.Sum().ToString();
+            averageActsLabel.Text = "Average Acts of Subterfuge per Asset: " + acts.Average().ToString("N2");
+            assetNameLabel.Text = "(Last Asset you Added: " + nameTextBox.Text + ")";
+
+            // Clear out text from the text boxes
+            nameTextBox.Text = "";
+            riggedTextBox.Text = "";
+            actsTextBox.Text = "";
+        }
+    }
+}
